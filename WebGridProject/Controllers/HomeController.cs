4823d142cef521238d988292380b03a23ad5a683
Echoneous <linn.devon@gmail.com>
2016-07-08 13:18:30 -0700

    TCS Assignment 1
    A basic one page web application which adds users to a database and
    displays them in a grid beneath the form. The database is able to be
    truncated via the "reset" button below the the grid.

---
+++ WebGridProject/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebGridProject.Models.DB;
+using WebGridProject.Models.EntityManager;
+
+namespace WebGridProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region form get and post for adding new user to db
+
+
+        //Get method
+        public ActionResult MyHomePage()
+        {
+            return View();
+        }
+        //Post method
+        [HttpPost]
+        public ActionResult MyHomePage(tblWebGrid TWG)
+        {
+            if(ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                UM.addNewUser(TWG);
+                ModelState.Clear();
+                return View();
+            }
+            return View();
+        }
+
+
+        /// <summary>
+        /// To Do:
+        /// 1.) Figure out how to have the object dynamically load into the view --- Check
+        /// 2.) Figure out how to truncate the table with a reset button
+        /// </summary>
+        /// <returns></returns>
+
+        #endregion
+
+
+        // View of rows in db that will show as a partial view list on page.
+        public ActionResult WeBGridView()
+        {
+            using (WebGridDBEntities db = new WebGridDBEntities())
+            {
+                return View(db.tblWebGrids.ToList());
+            }
+        }
+
+        // Reset table by deleting all rows
+        public ActionResult Reset()
+        {
+            using (WebGridDBEntities db = new WebGridDBEntities())
+            {
+                db.Database.ExecuteSqlCommand("TRUNCATE TABLE [WebGridTable]");
+                return RedirectToAction("MyHomePage", "Home");
+            }
+        }
+    }
+}
---
+++ WebGridProject/Models/EntityManager/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebGridProject.Models.DB;
+
+namespace WebGridProject.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void addNewUser(tblWebGrid AddNew)
+        {
+            using (WebGridDBEntities db = new WebGridDBEntities())
+            {
+                tblWebGrid TWG = new tblWebGrid();
+                TWG.Name = AddNew.Name;
+                TWG.DOB = AddNew.DOB;
+                TWG.AddressLine1 = AddNew.AddressLine1;
+                TWG.AddressLine2 = AddNew.AddressLine2;
+                TWG.City = AddNew.City;
+                TWG.State = AddNew.State;
+                TWG.Zip = AddNew.Zip;
+                TWG.Gender = AddNew.Gender;
+
+                db.tblWebGrids.Add(TWG);
+                db.SaveChanges();
+            }
+        }
+    }
+}
